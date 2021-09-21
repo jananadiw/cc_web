3cfd29bdfeb9cd6963e52a5bd91d0e59eee11d67
Jananadi Wedagedara <jananadi92@gmail.com>
2021-09-21 15:19:06 +0900

    refactor: Prepare loop through fetch data

--- pages/index.vue
+++ pages/index.vue
@@ -2,123 +2,11 @@
 <div class="page-wrapper">
  <div class="page-content">
     <div class="gallery">
-      <div class="gallery__item">
+     <div class="gallery__item" v-for="(item, index) in artDetails" :key="index">
         <a href="#" class="gallery__link">
-          <img src="~/assets/images/Surf_Crowd.jpg" class="gallery__image" />
+          <img :src="require(`~/assets/images/${item.name}.${item.ext}`)" class="gallery__image" />
           <div class="gallery__overlay">
-            <span>Surfers and others</span><br/>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Guitarist.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>The Naked Guitarist</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Fiji_From_Far.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Fuji from afar</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Forest.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Trees and sunlight</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/River_at_Sunset.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Riverside sunset</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Sunset_Kayaking.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>The Golden Kayaker</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Seascape.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Sparkling waves</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/sunriseSurfer.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>first wave</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Teal.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Teal</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Snowy_Arashiyama.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Snowy Arashiyama</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Water.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Reading by the stillness</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Sweet_Summer.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Sweet Summer</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Hug_Me.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>everyday hugs</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/SunsetFishing.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Fishmen at sunset</span>
-          </div>
-        </a>
-      </div>
-      <div class="gallery__item">
-        <a href="#" class="gallery__link">
-          <img src="~/assets/images/Climber.jpg" class="gallery__image" />
-          <div class="gallery__overlay">
-            <span>Solo Steps</span>
+            <span>{{item.caption}}</span><br/>
           </div>
         </a>
       </div>
@@ -131,9 +19,12 @@ import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component({})
 export default class Home extends Vue {
+  private artDetails = []; // define type; IArt
+
+  private async mounted(): Promise<void> {
+    this.artDetails = (await import('../test/mockData.json')).default as unknown as [];
+    console.log('arts', this.artDetails)
+  }
 
 }
 </script>
-<style lang="scss" scoped>
-
-</style>
